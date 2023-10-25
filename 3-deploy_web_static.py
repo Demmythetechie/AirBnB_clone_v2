734a77fd0b3da0f108575a8f5cc9d602881e7d4a
Demmythetechie <demmythetechie@gmail.com>
2023-10-25 20:34:49 +0100

    3. Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,7 +11,7 @@ from fabric.api import local, env, put, run
 from datetime import datetime
 from os.path import exists, isdir
 
-env.hosts = ['34.239.147.107', '52.3.247.207']
+env.hosts = ['34.202.159.191', '54.175.224.230']
 
 
 def do_pack():
