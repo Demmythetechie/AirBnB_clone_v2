9924b695daf1f034626d015382fd43f9283c17f5
Demmythetechie <demmythetechie@gmail.com>
2023-10-25 16:08:42 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """This script generates an archive from web_static dir."""
-from fabric import Connection, task
+from fabric.api import local
 from datetime import datetime
+from os.path import isdir
 
 
 @task
 def do_pack():
     """Archives the static files."""
+    time = datetime.now()
+    y = time.year()
+    o = time.month()
+    d = time.day()
+    h = time.hour()
+    m = time.minute()
+    s = time.second()
     try:
-        time = datetime.now()
-        y = time.year()
-        o = time.month()
-        d = time.day()
-        h = time.hour()
-        m = time.minute()
-        s = time.second()
         name = "web_static_{}{}{}{}{}{}".format(y, o, d, h, m, s)
-        lcl = Connection(host="localhost", user="demilade")
-        lcl.local('mkdir versions')
-        lcl.local('sudo apt -y install tar')
-        lcl.local("tar -cvf versions/{}.tgz web_static/*".format(name))
+        if isdir('versions') is False:
+            local('mkdir versions')
+        local('sudo apt -y install tar')
+        local("tar -cvf versions/{}.tgz web_static/*".format(name))
     except Exception:
         return None
